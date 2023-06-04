9c34f897587a0bf74836f0cf383f797419092d08
kwangminNam <johnpower@naver.com>
2023-06-04 23:54:54 +0900

    feat:add  next-auth login

---
+++ app/api/register/route.ts
@@ -0,0 +1,19 @@
+import bycrpt from 'bcrypt';
+import { NextResponse } from 'next/server';
+export async function POST(
+  request:Request
+){
+  const body = await request.json();
+  const { email , name, password } = body;
+
+  const hashedPassword = await bycrpt.hash(password,12);
+  const user = await prisma?.user.create({
+    data:{
+      email,
+      name,
+      hashedPassword
+    }
+  })
+
+  return NextResponse.json(user);
+}
